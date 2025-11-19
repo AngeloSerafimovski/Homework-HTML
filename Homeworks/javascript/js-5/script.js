0431bf4935728d02e0ba4fc0c62b574ea52c27f4
Angelo Serafimovski 24 <angeloserafimovskii@yahoo.com>
2025-11-19 06:31:00 -0500

    Dodadena js-5 DOM domasnoto

---
+++ Homeworks/javascript/js-5/script.js
@@ -0,0 +1,68 @@
+console.log(`Hello from ${navigator.userAgent}`);
+
+console.log(" --------------------------- ");
+
+console.log("Prvo");
+console.log("Vtoro");
+console.log("Treto");
+
+console.log(" ---------------------------- ");
+
+console.log(1);
+setTimeout(() =>{
+  console.log(2);
+},4200)
+  console.log(3);
+
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch((error) => console.log("Nastana greska: ", error))
+      .finally(() => console.log("Baranjeto zavrsi!"));
+
+  const titleE = document.getElementById("title");
+  titleE.style = "Color: red";
+
+
+const main = document.getElementsByClassName("content")
+main.style ="fontfamily: Verdana";
+main.style ="gap: 30px";
+
+
+const btns = document.querySelectorAll(".btn");
+console.log(btns);
+
+btns.forEach((btn) => {
+  btn.style.color = "red";
+  btn.style.padding = "8px 12px";
+
+  btn.addEventListener("click", () => {
+    console.log("clicked");
+  });
+});
+
+const paragraphs = document.querySelectorAll("p");
+console.log(paragraphs);
+
+document.querySelectorAll("p").forEach ( p => {
+  p.style.fontFamily = "Gill Sans";
+  p.style.fontWeight = "bold";
+})
+
+//znaci ako sakame da smenime samo eden odime obicno a ako sakame da gi pomineme site na pr paragrafi odime so foreach?
+// zosto vo negovo reshenie (res) e staveno vo zagrada
+
+ btn.addEventListener("mouseover", () => {
+    btn.classList.add("btn-hover");
+
+    setTimeout(() => {
+      btn.classList.remove("btn-hover");
+    }, 1500);
+  });
+
+ 
+
+
+
+
+

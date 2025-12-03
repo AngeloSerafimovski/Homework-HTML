b2c761eab9eceb7cd22dc0793d9fbdd5fe7f0cab
Angelo Serafimovski 24 <angeloserafimovskii@yahoo.com>
2025-12-03 12:56:08 -0500

    Domasno so casts , movies i shows renderirani,bez CSS

---
+++ Homeworks/javascript/js-9/script.js
@@ -0,0 +1,72 @@
+let shows = [];
+
+fetch("https://api.tvmaze.com/shows")
+  .then((res) => res.json())
+  .then((res) => {
+    shows = res;
+    console.log("ovoj su shows", shows);
+    renderShows(shows);
+  });
+
+function renderShows() {
+  const mainShowContainer = document.getElementById("shows-main-container");
+  mainShowContainer.innerHTML = "";
+
+  shows.forEach((show) => {
+    const showCard = document.createElement("div");
+    showCard.style.cursor = "pointer"
+    showCard.className = "show-card"
+
+    const showImg = document.createElement("img");
+    showImg.src = show.image.medium;
+    showCard.appendChild(showImg);
+    const showTitle = document.createElement("h2");
+    showTitle.innerText = show.name;
+    showCard.appendChild(showTitle);
+
+    const showGenr = document.createElement("h3");
+    showGenr.innerText = show.genres.join(", ");
+    showCard.appendChild(showGenr);
+
+    const showRait = document.createElement("h3");
+    showRait.innerText = show.rating.average;
+    showCard.appendChild(showRait);
+
+    const showView = document.createElement("a");
+   showView.href = `show.html?id=${show.id}`;
+   showView.innerText= "";
+     showView.innerText = "View";
+    showCard.addEventListener("click", () => {
+        console.log("Clicked");
+        
+         window.location.href = `show.html?id=${show.id}`
+    })
+    
+
+
+    showCard.appendChild(showView)
+    
+
+    mainShowContainer.appendChild(showCard);
+  });
+}
+
+function filterShows() {
+  const searchBar = document.getElementById("searchBar");
+  const searchBarinput = searchBar.value.trim().toLowerCase();
+  console.log("prebaruvam : ", searchBarinput);
+  const filteredShows = shows.filter((show) =>
+    show.name.toLowerCase().includes(searchBarinput)
+  );
+  console.log("Filtrirani :", filteredShows);
+  renderShows(filteredShows);
+  //   searchBar.value = "";
+}
+// za lajv da pisuvame
+document.getElementById("searchBar").addEventListener("input", filterShows);
+document
+  .querySelector("button[onclick='filterShows()']")
+  .addEventListener("click", () => {
+    filterShows();
+    document.getElementById("searchBar").value = "";
+  });
---
+++ Homeworks/javascript/js-9/show.js
@@ -0,0 +1,100 @@
+let shows = [];
+let episodes = [];
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+fetch(`https://api.tvmaze.com/shows/${id}`)
+  .then((res) => res.json())
+  .then((res) => {
+    shows = res;
+    console.log("ovoj su shows", shows);
+    renderShows(shows);
+  });
+
+function renderShows() {
+  const mainShowContainer = document.getElementById("shows-main-container");
+  mainShowContainer.innerHTML = "";
+
+  let show = shows;
+
+  const showCard = document.createElement("div");
+
+  const showImg = document.createElement("img");
+  showImg.src = show.image.medium;
+  showCard.appendChild(showImg);
+  const showTitle = document.createElement("h2");
+  showTitle.innerText = show.name;
+  showCard.appendChild(showTitle);
+
+  const showGenr = document.createElement("h3");
+  showGenr.innerText = show.genres.join(", ");
+  showCard.appendChild(showGenr);
+
+  const showRait = document.createElement("h3");
+  showRait.innerText = show.rating.average;
+  showCard.appendChild(showRait);
+
+  mainShowContainer.appendChild(showCard);
+}
+
+fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
+  .then((res) => res.json())
+  .then((res) => {
+    episodes = res;
+    console.log("ovoj su epizode", episodes);
+    renderEpisodes();
+  });
+ 
+function renderEpisodes() {
+  const mainEpisodeCont = document.getElementById("episodes-main-container");
+//   mainEpisodeCont.innerHTML = "";
+ 
+  episodes.forEach((epis) => {
+    const episCard = document.createElement("div");
+    episCard.classList.add("episode-card");
+    
+    const episImg = document.createElement("img");
+    episImg.src = epis.image.medium;
+    episCard.appendChild(episImg);
+    const episName = document.createElement("h2");
+    episName.innerText = epis.name;
+    episCard.appendChild(episName);
+   
+    const episSumm = document.createElement("p")
+    episSumm.innerHTML = epis.summary
+    episCard.appendChild(episSumm);
+
+ 
+    const showRait = document.createElement("h3");
+    showRait.innerText = epis.rating.average;
+    episCard.appendChild(showRait);
+ 
+    mainEpisodeCont.appendChild(episCard);
+  });
+}
+
+fetch(`https://api.tvmaze.com/shows/${id}/cast`)
+  .then((res) => res.json())
+  .then((res) => {
+    casts = res;
+    console.log("ovie se cast", casts);
+    renderCasts(casts);
+  });
+
+  function renderCasts(){
+    castContainer = document.getElementById("casts-main-container")
+
+    casts.forEach((cast)=>{
+        castContainer.innerHTML += `
+        <div class="cast-card">
+        <img src='${cast.person.image.medium}'/>
+       
+        <h2>Name: ${cast.person.name}</h2>
+        <h2>Character: ${cast.character.name}
+       
+ 
+        </div>
+        `
+    })
+  }
